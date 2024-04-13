2a9bec55d5d16c53f8a0322c84339624e1e126aa
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-04-13 22:17:06 +0900

    update: item을 싱글톤 패턴으로 사용하도록 변경

--- Assets/Scripts/Item_prototype/ItemManager.cs
+++ Assets/Scripts/Item_prototype/ItemManager.cs
@@ -7,16 +7,26 @@ using UnityEngine;
 /// </summary>
 public class ItemManager : MonoBehaviour
 {
-    public List<BaseItem> items;    
+    private static ItemManager _itemManager;              // 싱글톤 인스턴스
+    public static ItemManager Instance => _itemManager;   // 인스턴스 접근용 프로퍼티
+
+    public List<BaseItem> items;
     
-    void Awake()
-    {
+    void Awake(){
+        // 싱글톤 인스턴스 설정
+        if(_itemManager == null){
+            _itemManager = this;                            // 인스턴스 할당
+            DontDestroyOnLoad(gameObject);                  // 씬 전환 시에도 파괴되지 않도록 설정
+        }
+        else{
+            Destroy(gameObject);
+        }
+
         items = new List<BaseItem>();
         
         // 모든 BaseItem 타입의 객체를 찾아서 리스트에 추가
         BaseItem[] allBaseItems = Resources.FindObjectsOfTypeAll<BaseItem>();
-        foreach (BaseItem item in allBaseItems)
-        {
+        foreach (BaseItem item in allBaseItems){
             items.Add(item);
             ShowItem(item, true);
         }
@@ -26,8 +36,7 @@ public class ItemManager : MonoBehaviour
     /// 아이템 활성화 여부를 설정하는 함수
     /// </summary>
     /// <param name="show"></param>
-    public void ShowItem(BaseItem item, bool show)
-    {
+    public void ShowItem(BaseItem item, bool show){
         if(show){
             item.Activate();
         }
